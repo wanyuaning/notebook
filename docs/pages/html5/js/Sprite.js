b29d70942ed0c5161faa50d3e12f0f8eeed022e6
ewan <wane@qq.com>
2021-03-17 21:02:36 +0800

    v

--- docs/pages/html5/js/Sprite.js
+++ docs/pages/html5/js/Sprite.js
@@ -1,14 +1,36 @@
 class Sprite{
-  #o = {x:0, y:0, width: 0, height: 0, scale: [1, 1], rotate: 0, translate:[0, 0]}
-  constructor(options){
-    Object.assign(this.#o, options || {})
-    
-    
+  #CHILDREN   = {}
+  #TRANSFORM  = {x: 0, y: 0, scale: [1, 1], rotate: 0}
+  #TWEEN      = {x: 0, y: 0, scale: [1, 1], rotate: 0}
+  #autoWidth  = true
+  #autoHeight = true
+  constructor(x, y, width, height, origin){
+    this.x = x || 0
+    this.y = y || 0
+    this.width = width || 0                                                                                                     
+    this.height = height || 0 
+    this.origin = origin || [0, 0]
+    width && (this.#autoWidth = false) 
+    height && (this.#autoHeight = false)
   }
-  translate(x, y){}
-  translateTo(x, y){let arr = this.#o.translate; arr[0] = x; arr[1] = y}
-  rotate(deg){this.#o.rotate += deg; if(this.#o.rotate >= 360) this.#o.rotate -= 360}
-  rotateTo(deg){this.#o.rotate = deg}
+  addChild(name, child){
+    let tf = this.#TRANSFORM
+    this.#autoWidth && (this.width = Math.max(this.width, child.x + child.width))
+    this.#autoHeight && (this.height = Math.max(this.height, child.y + child.height))
+    this.#CHILDREN[name] = child
+  }
+  delChild(name){/*递归删除 之后补充*/delete this.#CHILDREN[name]}
+
+  translate  (x, y){let T = this.#TRANSFORM; T.x += x; T.y += y}
+  translateTo(x, y){let T = this.#TRANSFORM; T.x = x; T.y = y}
+  rotate  (deg){this.#TRANSFORM.rotate += deg}
+  rotateTo(deg){this.#TRANSFORM.rotate = deg}
+  scale  (x, y){let T = this.#TRANSFORM; T.scale[0] += x; T.scale[1] += y}
+  scaleTo(x, y){let T = this.#TRANSFORM; T.scale[0] = x; T.scale[1] = y}
+
+  update(){
+
+  }
   draw(){
     let {x, y, width, height, scale, rotate, translate} = this.#o
     stage.drawRect(x + translate[0], y + translate[1], width * scale[0], height * scale[1],{fillStyle:'#f00'})
