f831fbd27815f66d72ada328b5881ed732cd6c5b
ewan <wane@qq.com>
2021-04-22 21:01:22 +0800

    v

--- docs/pages/html5-2/js/Game.js
+++ docs/pages/html5-2/js/Game.js
@@ -19,7 +19,7 @@ class Game{
   changeSceneTo(name){let nextScene = this.sceneMap[name]; if(!nextScene) return; this.sceneCurrent.out(() => { nextScene.in(); this.sceneCurrent = this.sceneMap[name] })}
   transform(name, transform){ for (let key in transform) {this.actorMap[name][key](transform[key])} }
   getActor(name){ return this.actorMap[name] }
-  start(){this.#CONFIG.debug ? this.timer.testStart(this, {duration: 10000}) : this.timer.start(this) }
+  start(){this.timer.start(this) }
   draw(){
     let {showGrid, showRuler} = this.#CONFIG   
     this.stage.clean() // 1.清除画布    
--- docs/pages/html5-2/js/Timer.js
+++ docs/pages/html5-2/js/Timer.js
@@ -1,36 +1,31 @@
 
 class Timer{
-  #CONFIG = {FPS: 33.3333}
+  #CONFIG = {FPS: 33.3333, duration:5000}
   constructor(options){
     Object.assign(this.#CONFIG, options)
     this.#CONFIG.FPS < 33.3333 && (this.#CONFIG.FPS = 33.3333)
     this.time = 0
-    this.stop = true
-
-  }
-  testStart(game, {FPS, duration}){
-    this.stop = false
-    duration && setTimeout(()=>{this.stop = true}, duration)
-    
-    let timer = () => {
-      if(this.stop) return
-      game.draw()
-      window.setTimeout(timer, FPS || this.FPS)
-      
-    }
-    timer()
+    this.startTime = 0
+    this.state = 0 // 0stop 1play 2pause
+    this.duration = this.#CONFIG.duration
+    this.game = this.#CONFIG.game || {draw:() => {console.log(this.time)}}
   }
   start(game){
-    this.stop = false
+    this.startTime = new Date().getTime()
+    this.play()
+  }
+  play(){
+    this.time > this.duration && (this.state = 0)
+    this.state === 2 && (this.startTime = new Date().getTime() - this.time)
+    this.state = 1
     let timer = () => {
-      if(this.stop) return
-      game.draw()
+      if(this.state === 0) return
       this.time = new Date().getTime() - this.startTime
+      this.game.draw()
       window.setTimeout(timer, this.FPS)
-      
     }
     timer()
   }
-  pause(){this.stop = true}
-  stops(){this.stop = true; this.time = 0}
+  pause(){this.state = 2}
+  stop(){this.state = 0; this.time = 0}
 }  
